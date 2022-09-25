4c4eab8fe337f6a44c1e42b2e0fbcc74bab90240
Rohit <it.rohitverma@gmail.com>
2022-09-25 16:41:04 +0530

    client code added

--- intranet-client/src/App.js
+++ intranet-client/src/App.js
@@ -1,9 +1,45 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  // this is basically getter and setter of state variable 
+  const [user, setUser] = useState(null);
+  // life cycle hook - it will run the life cycle of component
+  useEffect(() => {
+    // below axios code is ajax(asyncronous javascript and xml) call 
+    axios.get('http://localhost:3001/getadmin').then((res) => {
+      console.log(res.data);
+      //i am setting user state here 
+      setUser(res.data);
+    });
+  }, []);
+
   return (
     <div>
-      this is react application
+      <div className='row bg-primary'>
+        <div className='col'>
+          Name
+        </div>
+        <div className='col'>
+          Age
+        </div>
+        <div className='col'>
+          Role
+        </div>
+      </div>
+      <div className='row bg-warning'>
+        <div className='col'>
+          {user.name}
+        </div>
+        <div className='col'>
+          {user.age}c
+        </div>
+        <div className='col'>
+          {user.role}
+        </div>
+      </div>
     </div>
   );
 }
